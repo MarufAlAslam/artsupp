d2b6b8a075c7566b6465118259d50a2a3ce52076
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-07-29 04:43:23 +0600

    almost done

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+    prevArrow: '<button class="btn prevBtn"><i class="fa fa-caret-left"></i></button>',
+    nextArrow: '<button class="btn nextBtn"><i class="fa fa-caret-right"></i></button>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
+
+$('.slider-1').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+    prevArrow: '<button class="btn prevBtn"><i class="fa fa-caret-left"></i></button>',
+    nextArrow: '<button class="btn nextBtn"><i class="fa fa-caret-right"></i></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
+
+
+
+$('.mapslider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
+
+
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+});
